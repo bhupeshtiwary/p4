71c86c3411db4f51def5b90115f92fff2c704ef6
Tiwary <bt21cse002@students.vnit.ac.in>
2025-04-27 21:30:00 +0000

    let see

--- echo_service.py
+++ echo_service.py
@@ -2,6 +2,6 @@ from scapy.all import *
 
 def echo_packet(pkt):
     # Send the packet back unchanged
-    sendp(pkt, iface="s1-eth0", verbose=True)
+    sendp(pkt, iface="svc1-eth0", verbose=True)
 
-sniff(iface="s1-eth0", prn=echo_packet)
+sniff(iface="svc1-eth0", prn=echo_packet)
--- sfc_topo.py
+++ sfc_topo.py
@@ -1,8 +1,10 @@
 from mininet.net import Mininet
 from mininet.node import Controller
+from mininet.node import OVSKernelSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink
+
 
 # Note: Assumes BMv2 switch is configured externally (e.g., via P4 Behavioral Model)
 class BMv2Switch(OVSKernelSwitch):
@@ -17,18 +19,29 @@ def sfcTopology():
     c0 = net.addController('c0')
 
     # Add BMv2 switch (requires external configuration with P4 program)
-    s1 = net.addSwitch('s1', cls=OVSKernelSwitch, dpid='1')
+    #s1 = net.addSwitch('s1', cls=BMv2Switch, dpid='1')
+
+    
+    s1 = net.addSwitch(
+        's1',
+        cls=BMv2Switch,
+        dpid='1',
+        sw_path='simple_switch_grpc',
+        json_path='sfc.json',
+        grpc_port=50051
+    )
+
 
     # Add hosts
     h1 = net.addHost('h1', ip='10.0.0.1/24')
-    s1_host = net.addHost('s1', ip='10.0.0.2/24')  # Service 1
-    s2_host = net.addHost('s2', ip='10.0.0.3/24')  # Service 2
+    svc1 = net.addHost('svc1', ip='10.0.0.2/24')  # Service 1
+    svc2 = net.addHost('svc2', ip='10.0.0.3/24')  # Service 2
     h2 = net.addHost('h2', ip='10.0.0.4/24')
 
     # Add links with specific ports
     net.addLink(h1, s1, port1=0, port2=1)
-    net.addLink(s1_host, s1, port1=0, port2=2)
-    net.addLink(s2_host, s1, port1=0, port2=3)
+    net.addLink(svc1, s1, port1=0, port2=2)
+    net.addLink(svc2, s1, port1=0, port2=3)
     net.addLink(h2, s1, port1=0, port2=4)
 
     # Start the network
